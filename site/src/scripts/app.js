5998b91fca0f8ca9350206ba42169c158ae8f5e9
Stefan Allchorne <allchorne39@sky.com>
2024-09-28 16:29:19 +0100

    Clear js and header/footer

--- site/src/scripts/app.js
+++ site/src/scripts/app.js
@@ -1,8 +1,3 @@
-const moment = require("moment") // For date formatting
-
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {    
     
-    // Get the current yeaer
-    document.getElementById('is-year').innerHTML = moment().year();
-
 })
